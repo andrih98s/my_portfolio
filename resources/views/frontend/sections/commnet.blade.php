193e2f850d518947645c5eccba7c4bc2ac45261b
Andrey Mokhammad <9andrih8@gmail.com>
2024-04-21 13:22:26 +0400

    Initial commit

---
+++ resources/views/frontend/sections/commnet.blade.php
@@ -0,0 +1,20 @@
+
+
+
+<!-- Testimonial-Area-Start -->
+<!-- @include('frontend.sections.testimonial')-->
+<!-- Testimonial-Area-End -->
+
+<!-- Blog-Area-Start -->
+<!--
+@include('frontend.sections.blog')
+-->
+<!-- Blog-Area-End -->
+
+
+
+
+<!-- Contact-Area-Start -->
+@include('frontend.sections.contact')
+
+<!-- Contact-Area-End -->
